d32a198b34ebc25f3b51ed2c03fe2c3947d4efbf
jjortiz016 <jjortiz016@misena.edu.co>
2025-05-26 17:17:03 -0500

    Se crearon los componentes topheader y home, se grego al archivo de rutas.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopheaderComponent } from "./components/common/topheader/topheader.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopheaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/common/topheader/topheader.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topheader',
+  imports: [],
+  templateUrl: './topheader.component.html',
+  styleUrl: './topheader.component.scss'
+})
+export class TopheaderComponent {
+
+}
