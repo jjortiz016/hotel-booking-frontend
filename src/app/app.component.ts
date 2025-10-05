eb4006bcd669177a9856f62f8f33f34674ff3411
jjortiz016 <jjortiz016@misena.edu.co>
2025-10-04 21:34:28 -0500

    Se creo componente navbar y su estructura html.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopheaderComponent } from "./components/common/topheader/topheader.component";
-
+import { NavbarComponent } from './components/common/navbar/navbar.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TopheaderComponent],
+  imports: [RouterOutlet, TopheaderComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
