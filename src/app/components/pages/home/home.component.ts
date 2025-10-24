d30c20e563a53c04a1a07ee167b1eecb1e31c0ee
jjortiz016 <jjortiz016@misena.edu.co>
2025-10-24 15:44:10 -0500

    Se agrego el componente datepicker

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { DpDatePickerModule } from 'ng2-date-picker';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+      DpDatePickerModule
+
+
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
+  datePickerConfig ={
+    format: 'YYYY-MM-DD'
+  }
 }
